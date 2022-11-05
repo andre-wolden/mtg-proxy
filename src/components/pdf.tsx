289a6813b775b2036b5b3c3d7726aeb9eaf35d3f
André Wolden <wolden.andre@gmail.com>
2022-11-05 21:57:30 +0100

    print a card to pdf and download

--- src/components/pdf.tsx
+++ src/components/pdf.tsx
@@ -1,23 +1,20 @@
 import { CardWithImage } from "./types";
+import jsPDF, { ImageOptions } from "jspdf";
 
 export const generatePdf = async (cards: CardWithImage[]) => {
-  // const doc = new jsPDF();
+  const doc = new jsPDF();
 
-  console.log("generating pdf");
+  if (cards && cards[0]) {
+    const imageOptions: ImageOptions = {
+      imageData: new Uint8Array(cards[0].image),
+      width: 63,
+      height: 88,
+      x: 30,
+      y: 30,
+    };
+    doc.addImage(imageOptions);
+  }
 
-  // cards.map(async (card) => {
-  //   const pattern = /^http:\/\/gatherer.wizards.com(.*)/;
-  //
-  //   const match = card.imageUrl.match(pattern);
-  //   const url = match ? match[1] : "no match";
-  //
-  //   console.log(url);
-  //
-  //   fetch(url)
-  //     .then((res) => res.text())
-  //     .then((res) => console.log(res));
-  // });
-
-  // doc.text("Hello world!", 10, 10);
-  // doc.save("a4.pdf");
+  doc.text("Hello world!", 10, 10);
+  doc.save("proxy-cards.pdf");
 };
